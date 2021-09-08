b6182d9ee7ab3d0636b9b7d5ed6fa2635da2173b
evilpeach <kanisorn@bandprotocol.com>
2021-09-09 02:32:27 +0700

    add to csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import time
 
 from twelvedata import get_twelvedata_price
 from yahoo_finance import get_yfinance_prices
@@ -33,15 +34,26 @@ SYMBOLS = [
 ]
 
 
-header = ["timestamp", "twelvedata"]
+header = ["timestamp", "twelvedata", "finage", "yahoo_finance"]
 
 print("start")
 
 twelvedata_prices = get_twelvedata_price(SYMBOLS)
-print(twelvedata_prices)
-
-yfinance_prices = get_yfinance_prices(SYMBOLS)
-print(yfinance_prices)
+# print(twelvedata_prices)
 
 finage_prices = get_finage_price((SYMBOLS))
-print(finage_prices)
+# print(finage_prices)
+
+yfinance_prices = get_yfinance_prices(SYMBOLS)
+# print(yfinance_prices)
+
+for symbol in SYMBOLS:
+    with open("./reports/" + symbol + ".csv", "a+", encoding="UTF8") as f:
+        writer = csv.writer(f)
+
+        # write the header
+        # writer.writerow(header)
+
+        # write the data
+        data = [int(time.time()), twelvedata_prices[symbol], finage_prices[symbol], yfinance_prices[symbol]]
+        writer.writerow(data)
